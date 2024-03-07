b1dcc5d831ca84c07bc27e011f573176f1a62aeb
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-03-07 10:44:11 +0800

    Get bot by id

--- Libs/RichillCapital.UseCases/Bots/BotDto.cs
+++ Libs/RichillCapital.UseCases/Bots/BotDto.cs
@@ -1,3 +1,10 @@
 namespace RichillCapital.UseCases.Bots;
 
-internal sealed record BotDto(string Id, string Name, string Description);
+internal sealed record BotDto(
+    string Id,
+    string Name,
+    string Description,
+    string Side,
+    string Platform,
+    DateTimeOffset CreatedAt,
+    DateTimeOffset UpdatedAt);
--- Libs/RichillCapital.UseCases/Bots/Get/GetBotByIdQueryHandler.cs
+++ Libs/RichillCapital.UseCases/Bots/Get/GetBotByIdQueryHandler.cs
@@ -1,4 +1,5 @@
 using RichillCapital.Domain;
+using RichillCapital.SharedKernel;
 using RichillCapital.SharedKernel.Monads;
 
 namespace RichillCapital.UseCases.Bots.Get;
@@ -7,10 +8,35 @@ internal sealed class GetBotByIdQueryHandler(
     IReadOnlyRepository<Bot> _botRepository) :
     IQueryHandler<GetBotByIdQuery, ErrorOr<BotDto>>
 {
-    public Task<ErrorOr<BotDto>> Handle(
+    public async Task<ErrorOr<BotDto>> Handle(
         GetBotByIdQuery query,
         CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var idResult = BotId.From(query.Id);
+
+        if (idResult.IsFailure)
+        {
+            return idResult.Error.ToErrorOr<BotDto>();
+        }
+
+        var maybeBot = await _botRepository.GetByIdAsync(idResult.Value, cancellationToken);
+
+        if (maybeBot.IsNull)
+        {
+            return Error.NotFound("Bot not found").ToErrorOr<BotDto>();
+        }
+
+        var bot = maybeBot.Value;
+
+        var botDto = new BotDto(
+            bot.Id.Value,
+            bot.Name.Value,
+            bot.Description.Value,
+            bot.Side.Name,
+            bot.Platform.Name,
+            bot.CreatedAt,
+            bot.UpdatedAt);
+
+        return botDto.ToErrorOr();
     }
 }
