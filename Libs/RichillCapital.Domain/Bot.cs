56183f45d0d35655ec5b40f04ea1a49167be8d93
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-03-07 10:26:57 +0800

    add signal

--- Libs/RichillCapital.Domain/Bot.cs
+++ Libs/RichillCapital.Domain/Bot.cs
@@ -5,6 +5,8 @@ namespace RichillCapital.Domain;
 
 public sealed class Bot : Entity<BotId>
 {
+    private readonly List<Signal> _signals = new();
+
     private Bot(
         BotId id,
         BotName name,
@@ -34,6 +36,8 @@ public sealed class Bot : Entity<BotId>
     public DateTimeOffset CreatedAt { get; private set; }
 
     public DateTimeOffset UpdatedAt { get; private set; }
+
+    public IReadOnlyList<Signal> Signals => _signals.AsReadOnly();
 
     public static ErrorOr<Bot> Create(
         BotId id,
@@ -67,5 +71,57 @@ public sealed class Bot : Entity<BotId>
     public void WithDescription(BotDescription description)
     {
         throw new NotImplementedException();
+    }
+}
+
+public sealed class Signal : ValueObject
+{
+    private Signal(
+        DateTimeOffset time,
+        TradeType tradeType,
+        Symbol symbol,
+        decimal quantity,
+        decimal price)
+    {
+        Time = time;
+        TradeType = tradeType;
+        Symbol = symbol;
+        Quantity = quantity;
+        Price = price;
+    }
+
+    public DateTimeOffset Time { get; private init; }
+
+    public TradeType TradeType { get; private init; }
+
+    public Symbol Symbol { get; private init; }
+
+    public decimal Quantity { get; private init; }
+
+    public decimal Price { get; private init; }
+
+    public static ErrorOr<Signal> Create(
+        DateTimeOffset time,
+        TradeType tradeType,
+        Symbol symbol,
+        decimal quantity,
+        decimal price)
+    {
+        return new Signal(
+            time,
+            tradeType,
+            symbol,
+            quantity,
+            price)
+            .ToErrorOr();
+    }
+
+    protected override IEnumerable<object> GetAtomicValues()
+    {
+        yield return Time;
+        yield return TradeType;
+        yield return Symbol;
+        yield return Quantity;
+        yield return Price;
     }
 }
